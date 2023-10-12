b0d6f9b677171efbe4ebbd28365629ac60b34e11
callumbennett <callumbennett@CALLUMBENNEB352>
2023-10-13 09:21:09 +1100

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Public Structure recHighScore
+        Public name As String
+        Public score As Integer
+    End Structure
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
